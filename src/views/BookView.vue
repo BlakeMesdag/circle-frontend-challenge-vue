885c1ff329fbc88e7643f890c1f78891610ce520
BlakeMesdag <696663+BlakeMesdag@users.noreply.github.com>
2025-01-18 11:17:09 -0500

    Some visual fixes on Book view

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -1,30 +1,31 @@
 <template>
-  <div v-if="book" class="container border rounded-2">
+  <div v-if="book" class="container">
     <div class="row p-2">
-      <div class="col-12 text-bg-secondary rounded-2 mb-2">
+      <div class="col-4 text-bg-secondary rounded-2 mb-2">
         <img style="height: 200px" />
       </div>
-    </div>
 
-    <div class="row">
-      <div class="col-12">
-        <h5 class="text-truncate">{{book.title}}</h5>
-        <h6>{{book.author}}</h6>
-      </div>
-    </div>
+      <div class="col-8">
+        <div class="container" style="height: 100%">
+          <div class="col-12" style="height: 100%; display: table">
 
-    <div class="row my-2 justify-content-end align-items-center">
-      <div class="col-12 align-items-start">
-        <span class="btn btn-primary shadow-sm position-relative">
-          Buy Now
-          <span
-            class="position-absolute top-0 start-95 translate-middle badge text-bg-danger"
-            v-if="book.availableStock < 10 && book.availableStock > 0">
-              {{book.availableStock + 1}} Left!
-          </span>
-        </span>
+            <div style="display: table-cell; vertical-align: middle;">
+              <h5 class="text-truncate">{{book.title}}</h5>
+              <h6>{{book.author}}</h6>
 
-        <button class="btn btn-danger" type="button" disabled v-if="!available">Sold Out</button>
+              <span class="btn btn-primary shadow-sm position-relative">
+                Buy Now
+                <span
+                  class="position-absolute top-0 start-95 translate-middle badge text-bg-danger"
+                  v-if="book.availableStock < 10 && book.availableStock > 0">
+                    {{book.availableStock + 1}} Left!
+                </span>
+              </span>
+
+              <button class="btn btn-danger" type="button" disabled v-if="!available">Sold Out</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
