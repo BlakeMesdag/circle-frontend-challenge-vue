b886f7e2308ac6a04a46bfe2053ce3c4e62347fb
BlakeMesdag <696663+BlakeMesdag@users.noreply.github.com>
2025-01-18 10:37:03 -0500

    Wrap book rendering to get rid of warnings, cleanup a bit

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="container border rounded-2">
+  <div v-if="book" class="container border rounded-2">
     <div class="row p-2">
       <div class="col-12 text-bg-secondary rounded-2 mb-2">
         <img style="height: 200px" />
       </div>
     </div>
+
     <div class="row">
       <div class="col-12">
         <h5 class="text-truncate">{{book.title}}</h5>
@@ -14,18 +15,14 @@
 
     <div class="row my-2 justify-content-end align-items-center">
       <div class="col-12 align-items-start">
-        <RouterLink
-          :to="`/books/${book.id}`"
-          book="{{book}}"
-          class="btn btn-primary shadow-sm position-relative"
-          v-if="available">
-        Buy Now
-          <span 
+        <span class="btn btn-primary shadow-sm position-relative">
+          Buy Now
+          <span
             class="position-absolute top-0 start-95 translate-middle badge text-bg-danger"
             v-if="book.availableStock < 10 && book.availableStock > 0">
               {{book.availableStock + 1}} Left!
           </span>
-        </RouterLink>
+        </span>
 
         <button class="btn btn-danger" type="button" disabled v-if="!available">Sold Out</button>
       </div>
